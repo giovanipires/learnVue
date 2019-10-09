8e1c311f9a0d063996f06fe8c407ed61f29d3803
Giovani Pires <giovani.pires@gmail.com>
2019-10-09 18:44:27 -0300

    Sixth commit, condicionais e listas;

---
+++ condicionais-e-listas/desafio-05/app.js
@@ -0,0 +1,22 @@
+new Vue({
+	el: '#desafio5',
+	data: {
+		alternar: true,
+		array: ['Pedro', 'Bia', 'Ana', 'Rebeca'],
+		livro: {
+			titulo: 'O Senhor dos Anéis',
+			autor: 'J.R.R. Tolkiens',
+			volume: '3'
+		},
+		aluno: {
+			id: 10,
+			nome: 'Maria',
+			notas: [7.67, 8.33, 6.98, 9.21]
+		},
+		animais: [
+			{nome: 'Arghus', idade: 7, peso: 13},
+			{nome: 'Tutu', idade: 7, peso: 6},
+			{nome: 'Harley', idade: 1, peso: 2},
+		]
+	}
+})
