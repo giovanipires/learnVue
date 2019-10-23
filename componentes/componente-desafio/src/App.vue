e1dd03ce518d2634e8d26e042d572bf6f4d4f146
Giovani Pires <giovani.pires@gmail.com>
2019-10-23 16:26:23 -0300

    entendendo o funcionamento dos componentes no Vue;

---
+++ componentes/componente-desafio/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+	<v-app>
+		<app-menu/>
+		<app-toolbar/>
+		<app-caroussel/>
+		<app-footer/>
+	</v-app>
+</template>
+
+<script>
+import Footer from './Footer.vue'
+import Toolbar from './Toolbar.vue'
+import Menu from './Menu.vue'
+import Caroussel from './Caroussel.vue'
+export default {
+	components: { 
+		'app-footer': Footer,
+		'app-toolbar': Toolbar,
+		'app-menu': Menu,
+		'app-caroussel': Caroussel
+	},
+}
+</script>
