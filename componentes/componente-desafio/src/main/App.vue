f82592f93466f433cd7081e531c4d538f600fe43
Giovani Pires <giovani.pires@gmail.com>
2019-10-25 09:10:13 -0300

    módulo componentes, commit;

---
+++ componentes/componente-desafio/src/main/App.vue
@@ -0,0 +1,23 @@
+<template>
+	<v-app>
+		<app-menu/>
+		<app-toolbar/>
+		<app-caroussel/>
+		<app-footer/>
+	</v-app>
+</template>
+
+<script>
+import Footer from '@/components/template/Footer.vue'
+import Toolbar from '@/components/template/Toolbar.vue'
+import Menu from '@/components/template/Menu.vue'
+import Caroussel from '@/components/widgets/Caroussel.vue'
+export default {
+	components: { 
+		appFooter: Footer,
+		appToolbar: Toolbar,
+		appMenu: Menu,
+		appCaroussel: Caroussel
+	},
+}
+</script>
