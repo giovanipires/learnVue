13aef66e2280b210f1b5886293c1cb3a761b116f
Giovani Pires <giovani.pires@gmail.com>
2019-10-25 15:32:13 -0300

    Aula 121, eventos personalizados, commit;

---
+++ componentes/componentes-comunicacao/comunicacao/src/components/UsuarioInfo.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="componente">
+        <h2>As Informações de Usuário</h2>
+        <p>Vários detalhes...</p>
+        <!-- <p>Nome do usuário: <strong>{{ nome }}</strong></p> -->
+        <p>Nome invertido: <strong>{{ inverterNome() }}</strong></p>
+        <button @click="reiniciarNome">Reiniciar Nome</button>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        nome: {
+            type: String,
+            require: true,
+            defaut: 'Anônimo'
+            // default: function() {
+            //     return Array(10).fill(0).join(',')
+            // }
+        },
+        perfil: String
+    },
+    methods: {
+        inverterNome() {
+            return this.nome.split('').reverse().join('')
+        },
+        reiniciarNome() {
+            this.nome = 'Pedro'
+            this.$emit('nomeMudou', this.nome)
+        },
+    },
+}
+</script>
+
+<style scoped>
+    .componente {
+        flex: 1;
+        background-color: #ec485f;
+        color: #fff;
+    }
+</style>
