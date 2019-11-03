0f973b603d323d6540d07493e35858ed685b2004
Giovani Pires <giovani.pires@gmail.com>
2019-11-03 08:24:18 -0300

    aula 262, router, redirect;

---
+++ routes/rotas-exercicios/src/components/usuario/UsuarioDetalhe.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="usuario-detalhe">
+     <h3>Usuário detalhe</h3>
+     <p><strong>Código:</strong> {{ id }}</p>
+      <!-- :to="`/usuario/${id}/editar`" -->
+      <!-- :to="{ name: 'editarUsuario', parms: { id: $route.params.id }}" -->
+     <router-link tag="button" primario 
+      :to="{ name: 'editarUsuario', parms: { id },
+        query:{completo: true, ligua:pt}}"
+     >
+       Editar
+     </router-link>
+  </div>
+</template>
+
+<script>
+export default {
+   props: ['id'],
+  // data() {
+  //   return {
+  //     id: this.$route.params.id
+  //   }
+  // },
+  // watch: {
+  //   $route(to, from) {
+  //     this.id = to.params.id
+  //   }
+  // },
+}
+</script>
+
+<style>
+
+</style>
---
+++ routes/rotas-exercicios/src/router.js
@@ -0,0 +1,42 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Inicio from './components/Inicio'
+import Menu from './components/template/Menu'
+import MenuAlt from './components/template/MenuAlt'
+import Usuario from './components/usuario/Usuario'
+import UsuarioLista from './components/usuario/UsuarioLista'
+import UsuarioDetalhe from './components/usuario/UsuarioDetalhe'
+import UsuarioEditar from './components/usuario/UsuarioEditar'
+
+
+Vue.use(Router)
+
+export default new Router({
+   mode: 'history',
+   routes: [{
+      name: 'inicio',
+      path: '/',
+      // component: Inicio
+      components: {
+         default: Inicio,
+         menu: Menu
+      }
+   }, {
+      path: '/usuario',
+      // component: Usuario,
+      components: {
+         default: Usuario,
+         menu: MenuAlt,
+         menuInferior: MenuAlt
+      },
+      props: true,
+      children: [
+         { path: '', compoment: UsuarioLista},
+         { path: ':id', compoment: UsuarioDetalhe, props: true},
+         { path: ':id/editar', compoment: UsuarioEditar, props: true, name: 'editarUsuario'},
+      ]
+   }, {
+      path: '/redirecionar',
+      redirect: '/usuario'
+   }]
+})
